f9eea9dfea54113c963089713c6e5ddd1727ec6e
VictorHugo1607ESIME <victor.hugo.mr1607@gmail.com>
2023-02-06 23:23:44 -0600

    lupa y app(JS)

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+const searchIcon = document.getElementById('search_icon')
+
+function changeIconSearchWhite(){
+    searchIcon.src = "../icons/lupa-white.png"
+}
+
+function changeIconSearch(){
+    searchIcon.src = "../icons/lupa.png"
+}
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid col-md-3 justify-content-center">
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-danger" type="submit"><img src="{{ asset('icons/lupa.png') }}"    /></button>
+        <button class="btn btn-outline-danger" onmouseover="changeIconSearchWhite()" onmouseout="changeIconSearch()" type="submit"><img id="search_icon" src="{{ asset('icons/lupa.png') }}"    /></button>
       </form>
     </div>
     <div class="col-md-3 d-flex justify-content-end">
