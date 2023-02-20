7b4f032234321838efbc1ff3f23c5ed0ed6b4c7f
VictorHugo1607ESIME <victor.hugo.mr1607@gmail.com>
2023-02-20 13:42:31 -0600

    gallery home

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -9,10 +9,13 @@
     <div class="col-12 mt-4 mb-4">
         <h3 class="titleSection">Las mejores fotos de los espectáculos</h3>
     </div>
-    <div class="row">
+    <div class="row d-flex justify-content-between">
 
         <a href="" class="contentImgGeneral contentImgWibe">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM73877561_-960x640.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Nombre del album</label>
@@ -21,6 +24,9 @@
 
         <a href="" class="contentImgGeneral contentImgLong">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM72847458_-960x1440.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Angelique Boyer</label>
@@ -29,6 +35,9 @@
 
         <a href="" class="contentImgGeneral contentImgLong">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM58296003_-960x1440.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Alfombra de la Nueva Película Maquíllame Otra Vez</label>
@@ -37,6 +46,9 @@
 
         <a href="" class="contentImgGeneral contentImgWibe">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM72007374_-960x640.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Pepe Aguilar Presenta Jaripeo Sin Fronteras en la Plaza de Toros México</label>
@@ -45,6 +57,9 @@
 
         <a href="" class="contentImgGeneral contentImgWibe">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM70107184_-960x640.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Celebridades en la Alfombra Roja de la Serie “La Cabeza de Joaquín Murrieta”</label>
@@ -53,6 +68,9 @@
 
         <a href="" class="contentImgGeneral contentImgLong">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/AMC54385612_-960x1440.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Manoella Torres se Presentará en el Lunario del Auditorio de la CDMX</label>
@@ -61,6 +79,9 @@
 
         <a href="" class="contentImgGeneral contentImgWibe">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/01/ENTREFAM50305204_-960x640.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Susana Zavaleta Participará en un Recital con el Tenor Andrea Bocelli</label>
@@ -69,6 +90,9 @@
 
         <a href="" class="contentImgGeneral contentImgLong">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/AMC54255599_-960x1440.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Conferencia de Prensa con Jesse & Joy por su Próxima Participación en el Auditorio Nacional</label>
@@ -77,6 +101,9 @@
 
         <a href="" class="contentImgGeneral contentImgWibe">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/AMC54505624_-960x640.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Coque Muñiz en Conferencia por su Presentación en el Lunario</label>
@@ -85,6 +112,9 @@
 
         <a href="" class="contentImgGeneral contentImgLong">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/01/AMC_20150325_3774-960x1373.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Fallece Polo Polo a los 78 Años de Edad</label>
@@ -93,6 +123,9 @@
 
         <a href="" class="contentImgGeneral contentImgLong">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/01/ENTREFAM43494523_-960x1440.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Alfombra Roja de la Película Infelices Para Siempre</label>
@@ -101,6 +134,9 @@
 
         <a href="" class="contentImgGeneral contentImgWibe">
             <img class="imgAlbum" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM51515325_-960x640.jpg" alt="">
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12" for="">Fecha 16-02-2023</label>
                 <label class="titleAlbum col-sm-12" for="">Enrique Guzmán Presenta su Disco Titulado Soy Yo</label>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <style>
     .contentMainsPhotosHeigth{
-        height: 65%;
+        height: 75%;
         overflow: hidden;
     }
 
@@ -16,18 +16,24 @@
 @extends('layout.app')
 @section('content')
 
-    <div class="contentMainsPhotosHeigth mb-2 mt-2 col-sm-12 d-flex justify-content-between row">
+    <div class="contentMainsPhotosHeigth mb-2 mt-2 d-flex justify-content-between row">
 
-        <a href="" class="mainContentPhoto col-md-8">
+        <a href="" class="mainContentPhoto col-md-9">
             <img class="stylePhotos" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM59036077_-960x640.jpg" >
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12">Fecha: 12 - 08 - 2023</label>
                 <label class="titleAlbum col-sm-12">Diego Boneta Lanza su Marca de Tequila “Defrente”</label>
             </div>
         </a>
 
-        <a href="" class="mainContentPhoto col-md-4">
+        <a href="" class="mainContentPhoto col-md-3">
             <img class="stylePhotos col-md-12" src="https://entrefam.com/wp-content/uploads/2023/02/ENTREFAM58376011_-960x1440.jpg" >
+            <div class="contentNumberImage col-12 row">
+                <label class="numberImage col-12" for=""><img class="iconNumberPhotos" src="{{ asset('icons/imageIcon.png') }}" alt=""> 10 imágenes</label>
+            </div>
             <div class="contentInfoAlbum column">
                 <label class="titleAlbum col-sm-12">Fecha: 12 - 08 - 2023</label>
                 <label class="titleAlbum col-sm-12">Conferencia de Prensa del Programa “La Corneta”</label>
