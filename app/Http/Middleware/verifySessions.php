a8c78650080be7b762d5af1a74ab71355db649ce
Raul Arellano Guevara <raularellanoguevara@gmail.com>
2023-03-14 03:02:28 -0600

    update

---
+++ app/Http/Middleware/verifySessions.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class verifySessions
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = new User();
+        $user->verify_sessions();
+        if (session('logout')) {
+            return redirect(URL('/logout'));
+
+            ///cerrar sesion o enviar a pagiena para indicar que se cerrara la sesion
+        }
+        return $next($request);
+    }
+}
---
+++ app/Models/Albums.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Libs\helpers;
+
+class Albums extends Model
+{
+    use HasFactory;
+    public help;
+    public function __construct()
+    {
+        $this->help = new helpers();
+    }
+    public function add($name,$date=null,$cont=0)
+    {
+        try {
+        $insert_id = DB::table('albums')->insertGetId([
+            'album_name' => trim($name),
+            'album_slug' => $this->help->get_slug($name),
+            'album_status'=>'A',
+            'number_photos'=>$cont,
+            'date'=>$date,
+            'company_id'=>session('company_id')
+        ]);
+        return $insert_id;
+        } catch (\Throwable $th) {
+            //throw $th;
+            return 0;
+        }
+    }
+    public function edit($id,$name,$date=null,$cont=0)
+    {
+        try {
+        $update = DB::table('albums')->where('id',$id)->update([
+            'album_name' => trim($name),
+            'album_slug' => $this->help->get_slug($name),
+            'album_status'=>'A',
+            'number_photos'=>$cont,
+            'date'=>$date,
+        ]);
+        return $id;
+        } catch (\Throwable $th) {
+            //throw $th;
+            return 0;
+        }
+    }
+
+    public function set_status($id)
+    {
+        try {
+        $status='A';
+        $row = DB::table('albums')->where('id',$id)->first();
+        if ($row->status='A') {
+            $status='E';
+        }
+        $update = DB::table('albums')->where('id',$id)->update([
+            'album_status'=>$status,
+        ]);
+        return true;
+    } catch (\Throwable $th) {
+        //throw $th;
+        return false;
+    }
+    }
+}
